7e6351d2a0e8e102222ec75420aa532b94e39f8b
imagee <51212151+realimposter@users.noreply.github.com>
2023-07-07 11:05:48 -0700

    Update forward_warp_cuda_kernel.cu

--- Forward_Warp/cuda/forward_warp_cuda_kernel.cu
+++ Forward_Warp/cuda/forward_warp_cuda_kernel.cu
@@ -48,36 +48,37 @@ __global__ void forward_warp_cuda_forward_kernel(
         sort[index] = flow_amplitude;
 
         if (interpolation_mode == GridSamplerInterpolation::Bilinear) {
-        const int x_f = static_cast<int>(::floor(x));
-        const int y_f = static_cast<int>(::floor(y));
-        const int x_c = x_f + 1;
-        const int y_c = y_f + 1;
-        if(x_f>=0 && x_c<W && y_f>=0 && y_c<H){
-            const scalar_t nw_k = (x_c - x) * (y_c - y);
-            const scalar_t ne_k = (x - x_f) * (y_c - y);
-            const scalar_t sw_k = (x_c - x) * (y - y_f);
-            const scalar_t se_k = (x - x_f) * (y - y_f);
-            const scalar_t* im0_p = im0+get_im_index(b, 0, h, w, C, H, W);
-            scalar_t* im1_p = im1+get_im_index(b, 0, y_f, x_f, C, H, W);
-            for (int c = 0; c < C; ++c, im0_p+=H*W, im1_p+=H*W){
-                atomicAdd(im1_p,     nw_k*(*im0_p));
-                atomicAdd(im1_p+1,   ne_k*(*im0_p));
-                atomicAdd(im1_p+W,   sw_k*(*im0_p));
-                atomicAdd(im1_p+W+1, se_k*(*im0_p));
+            const int x_f = static_cast<int>(::floor(x));
+            const int y_f = static_cast<int>(::floor(y));
+            const int x_c = x_f + 1;
+            const int y_c = y_f + 1;
+            if(x_f>=0 && x_c<W && y_f>=0 && y_c<H){
+                const scalar_t nw_k = (x_c - x) * (y_c - y);
+                const scalar_t ne_k = (x - x_f) * (y_c - y);
+                const scalar_t sw_k = (x_c - x) * (y - y_f);
+                const scalar_t se_k = (x - x_f) * (y - y_f);
+                const scalar_t* im0_p = im0+get_im_index(b, 0, h, w, C, H, W);
+                scalar_t* im1_p = im1+get_im_index(b, 0, y_f, x_f, C, H, W);
+                for (int c = 0; c < C; ++c, im0_p+=H*W, im1_p+=H*W){
+                    *(im1_p) = nw_k*(*im0_p);
+                    *(im1_p+1) = ne_k*(*im0_p);
+                    *(im1_p+W) = sw_k*(*im0_p);
+                    *(im1_p+W+1) = se_k*(*im0_p);
+                }
             }
-        }
         } 
         else if (interpolation_mode == GridSamplerInterpolation::Nearest) {
-        const int x_nearest = static_cast<int>(::round(x));
-        const int y_nearest = static_cast<int>(::round(y));
-        if(x_nearest>=0 && x_nearest<W && y_nearest>=0 && y_nearest<H){
-            const scalar_t* im0_p = im0+get_im_index(b, 0, h, w, C, H, W);
-            scalar_t* im1_p = im1+get_im_index(b, 0, y_nearest, x_nearest, C, H, W);
-            for (int c = 0; c < C; ++c, im0_p += H*W, im1_p += H*W) {
-                *im1_p = *im0_p;
+            const int x_nearest = static_cast<int>(::round(x));
+            const int y_nearest = static_cast<int>(::round(y));
+            if(x_nearest>=0 && x_nearest<W && y_nearest>=0 && y_nearest<H){
+                const scalar_t* im0_p = im0+get_im_index(b, 0, h, w, C, H, W);
+                scalar_t* im1_p = im1+get_im_index(b, 0, y_nearest, x_nearest, C, H, W);
+                for (int c = 0; c < C; ++c, im0_p += H*W, im1_p += H*W) {
+                    *im1_p = *im0_p;
+                }
             }
         }
-        }
+
     }
   }
 }
