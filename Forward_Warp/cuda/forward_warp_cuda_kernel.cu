492ae3d12bbb0057d122a0707f37445f822e0a4a
imagee <51212151+realimposter@users.noreply.github.com>
2023-07-07 08:17:38 -0700

    Update forward_warp_cuda_kernel.cu

--- Forward_Warp/cuda/forward_warp_cuda_kernel.cu
+++ Forward_Warp/cuda/forward_warp_cuda_kernel.cu
@@ -27,12 +27,13 @@ __global__ void forward_warp_cuda_forward_kernel(
     const scalar_t* im0,
     const scalar_t* flow,
     scalar_t* im1,
-    scalar_t* sort,
     const int B,
     const int C,
     const int H,
     const int W,
     const GridSamplerInterpolation interpolation_mode) {
+  // CUDA_KERNEL_LOOP(index, total_step-1) {
+  // bug fix, thx to @tkkcc
   CUDA_KERNEL_LOOP(index, total_step) {
     const int b = index / (H * W);
     const int h = (index-b*H*W) / W;
@@ -51,25 +52,11 @@ __global__ void forward_warp_cuda_forward_kernel(
         const scalar_t se_k = (x - x_f) * (y - y_f);
         const scalar_t* im0_p = im0+get_im_index(b, 0, h, w, C, H, W);
         scalar_t* im1_p = im1+get_im_index(b, 0, y_f, x_f, C, H, W);
-        scalar_t* sort_p = sort+get_im_index(b, 0, y_f, x_f, C, H, W);
-        for (int c = 0; c < C; ++c, im0_p+=H*W, im1_p+=H*W, sort_p+=H*W){
-          const scalar_t curr_sort = *sort_p;
-          if (curr_sort < nw_k) {
-            *sort_p = nw_k;
-            *im1_p = nw_k * (*im0_p);
-          }
-          if (curr_sort < ne_k) {
-            *sort_p = ne_k;
-            *(im1_p+1) = ne_k * (*im0_p);
-          }
-          if (curr_sort < sw_k) {
-            *sort_p = sw_k;
-            *(im1_p+W) = sw_k * (*im0_p);
-          }
-          if (curr_sort < se_k) {
-            *sort_p = se_k;
-            *(im1_p+W+1) = se_k * (*im0_p);
-          }
+        for (int c = 0; c < C; ++c, im0_p+=H*W, im1_p+=H*W){
+            atomicAdd(im1_p,     nw_k*(*im0_p));
+            atomicAdd(im1_p+1,   ne_k*(*im0_p));
+            atomicAdd(im1_p+W,   sw_k*(*im0_p));
+            atomicAdd(im1_p+W+1, se_k*(*im0_p));
         }
       }
     } 
@@ -79,13 +66,8 @@ __global__ void forward_warp_cuda_forward_kernel(
       if(x_nearest>=0 && x_nearest<W && y_nearest>=0 && y_nearest<H){
         const scalar_t* im0_p = im0+get_im_index(b, 0, h, w, C, H, W);
         scalar_t* im1_p = im1+get_im_index(b, 0, y_nearest, x_nearest, C, H, W);
-        scalar_t* sort_p = sort+get_im_index(b, 0, y_nearest, x_nearest, C, H, W);
-        for (int c = 0; c < C; ++c, im0_p += H*W, im1_p += H*W, sort_p += H*W) {
-          const scalar_t curr_sort = *sort_p;
-          if (curr_sort < 1) {
-            *sort_p = 1;
+        for (int c = 0; c < C; ++c, im0_p += H*W, im1_p += H*W) {
             *im1_p = *im0_p;
-          }
         }
       }
     }
@@ -127,4 +109,89 @@ __global__ void forward_warp_cuda_backward_kernel(
         for (int c = 0; c < C; ++c, im0_grad_p+=H*W){
           const scalar_t nw_grad = grad_output[get_im_index(b, c, y_f, x_f, C, H, W)];
           const scalar_t ne_grad = grad_output[get_im_index(b, c, y_f, x_c, C, H, W)];
-          const scalar_t sw_grad = grad_output[get_im_index(b, c, y_c,
+          const scalar_t sw_grad = grad_output[get_im_index(b, c, y_c, x_f, C, H, W)];
+          const scalar_t se_grad = grad_output[get_im_index(b, c, y_c, x_c, C, H, W)];
+          const scalar_t p = im0[get_im_index(b, c, h, w, C, H, W)];
+          atomicAdd(im0_grad_p, nw_k*nw_grad);
+          atomicAdd(im0_grad_p, ne_k*ne_grad);
+          atomicAdd(im0_grad_p, sw_k*sw_grad);
+          atomicAdd(im0_grad_p, se_k*se_grad);
+          flow_grad_x -= (y_c-y)*p*nw_grad;
+          flow_grad_y -= (x_c-x)*p*nw_grad;
+          flow_grad_x += (y_c-y)*p*ne_grad;
+          flow_grad_y -= (x-x_f)*p*ne_grad;
+          flow_grad_x -= (y-y_f)*p*sw_grad;
+          flow_grad_y += (x_c-x)*p*sw_grad;
+          flow_grad_x += (y-y_f)*p*se_grad;
+          flow_grad_y += (x-x_f)*p*se_grad;
+        }
+        flow_grad[index*2+0] = flow_grad_x;
+        flow_grad[index*2+1] = flow_grad_y;
+      }
+    } 
+    else if (interpolation_mode == GridSamplerInterpolation::Nearest) {
+      const int x_nearest = static_cast<int>(::round(x));
+      const int y_nearest = static_cast<int>(::round(y));
+      if(x_nearest>=0 && x_nearest<W && y_nearest>=0 && y_nearest<H){
+        scalar_t* im0_grad_p = im0_grad+get_im_index(b, 0, h, w, C, H, W);
+        const scalar_t* im1_grad_p = grad_output+get_im_index(b, 0, y_nearest, x_nearest, C, H, W);
+        for (int c = 0; c < C; ++c, im0_grad_p += H*W, im1_grad_p += H*W) {
+            *im0_grad_p = *im1_grad_p;
+        }
+      }
+    }
+  }
+}
+
+at::Tensor forward_warp_cuda_forward(
+    const at::Tensor im0, 
+    const at::Tensor flow,
+    const GridSamplerInterpolation interpolation_mode) {
+  auto im1 = at::zeros_like(im0);
+  const int B = im0.size(0);
+  const int C = im0.size(1);
+  const int H = im0.size(2);
+  const int W = im0.size(3);
+  const int total_step = B * H * W;
+  AT_DISPATCH_FLOATING_TYPES(im0.scalar_type(), "forward_warp_forward_cuda", ([&] {
+    forward_warp_cuda_forward_kernel<scalar_t>
+    <<<GET_BLOCKS(total_step), CUDA_NUM_THREADS>>>(
+      total_step,
+      im0.data<scalar_t>(),
+      flow.data<scalar_t>(),
+      im1.data<scalar_t>(),
+      B, C, H, W,
+      interpolation_mode);
+  }));
+
+  return im1;
+}
+
+std::vector<at::Tensor> forward_warp_cuda_backward(
+    const at::Tensor grad_output,
+    const at::Tensor im0, 
+    const at::Tensor flow,
+    const GridSamplerInterpolation interpolation_mode) {
+  auto im0_grad = at::zeros_like(grad_output);
+  auto flow_grad = at::empty_like(flow);
+  const int B = im0.size(0);
+  const int C = im0.size(1);
+  const int H = im0.size(2);
+  const int W = im0.size(3);
+  const int total_step = B * H * W;
+
+  AT_DISPATCH_FLOATING_TYPES(grad_output.type(), "forward_warp_backward_cuda", ([&] {
+    forward_warp_cuda_backward_kernel<scalar_t>
+    <<<GET_BLOCKS(total_step), CUDA_NUM_THREADS>>>(
+      total_step,
+      grad_output.data<scalar_t>(),
+      im0.data_ptr<scalar_t>(),
+      flow.data<scalar_t>(),
+      im0_grad.data<scalar_t>(),
+      flow_grad.data<scalar_t>(),
+      B, C, H, W,
+      interpolation_mode);
+  }));
+
+  return {im0_grad, flow_grad};
+}
