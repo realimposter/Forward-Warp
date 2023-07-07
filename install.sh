cadc6f080c2d22bc033ca86c4f02d8afe8a455d3
imagee <51212151+realimposter@users.noreply.github.com>
2023-07-07 10:14:09 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 work_path=$(dirname $(readlink -f $0))
 cd ${work_path}/Forward_Warp/cuda/
 conda activate pytorch
-python setup.py install --verbose 2>&1 | tee setup.log
+python setup.py install --verbose 2>&1 | tee /var/log/setup.log
 cd ../../
 python setup.py install
