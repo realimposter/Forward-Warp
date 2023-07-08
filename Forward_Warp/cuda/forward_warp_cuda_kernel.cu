facbda0df44358f3839ee485e7440dbf0503165a
imagee <51212151+realimposter@users.noreply.github.com>
2023-07-07 18:55:17 -0700

    Update forward_warp_cuda_kernel.cu

--- Forward_Warp/cuda/forward_warp_cuda_kernel.cu
+++ Forward_Warp/cuda/forward_warp_cuda_kernel.cu
@@ -59,10 +59,23 @@ __global__ void forward_warp_cuda_forward_kernel(
                     const scalar_t* im0_p = im0+get_im_index(b, 0, h, w, C, H, W);
                     scalar_t* im1_p = im1+get_im_index(b, 0, y_f, x_f, C, H, W);
                     for (int c = 0; c < C; ++c, im0_p+=H*W, im1_p+=H*W){
-                        atomicAdd(im1_p,     nw_k*(*im0_p));
-                        atomicAdd(im1_p+1,   ne_k*(*im0_p));
-                        atomicAdd(im1_p+W,   sw_k*(*im0_p));
-                        atomicAdd(im1_p+W+1, se_k*(*im0_p));
+                        float new_val;
+
+                        new_val = static_cast<float>(nw_k*(*im0_p));
+                        atomicExch((float*)im1_p, new_val);
+                        *im1_p = static_cast<scalar_t>(*im1_p);
+
+                        new_val = static_cast<float>(ne_k*(*im0_p));
+                        atomicExch((float*)(im1_p+1), new_val);
+                        *(im1_p+1) = static_cast<scalar_t>(*(im1_p+1));
+
+                        new_val = static_cast<float>(sw_k*(*im0_p));
+                        atomicExch((float*)(im1_p+W), new_val);
+                        *(im1_p+W) = static_cast<scalar_t>(*(im1_p+W));
+
+                        new_val = static_cast<float>(se_k*(*im0_p));
+                        atomicExch((float*)(im1_p+W+1), new_val);
+                        *(im1_p+W+1) = static_cast<scalar_t>(*(im1_p+W+1));
                     }
                 }
             } 
