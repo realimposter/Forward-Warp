3815707baa53e4bab91aa0a7da86653dbc9bdb44
imagee <51212151+realimposter@users.noreply.github.com>
2023-07-11 21:02:26 -0700

    Update forward_warp_cuda_kernel.cu

--- Forward_Warp/cuda/forward_warp_cuda_kernel.cu
+++ Forward_Warp/cuda/forward_warp_cuda_kernel.cu
@@ -188,41 +188,59 @@ __global__ void inpaint_nan_pixels_kernel(
     const int total_step = B * C * H * W;
     const int radius = 2;  // Add this line to define radius
 
-    for (int iteration = 0; iteration < 5; ++iteration) {
+    __shared__ scalar_t shared_im1[THREADS_PER_BLOCK];  // Shared memory for neighboring pixels
+    const int num_blocks = (total_step + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
+
+    for (int iteration = 0; iteration < 10; ++iteration) {
         bool has_nan = false;  // Track if NaN pixels are found in the iteration
 
-        CUDA_KERNEL_LOOP(index, total_step) {
-            if (!isnan(im1[index])) continue;
+        for (int block_idx = 0; block_idx < num_blocks; ++block_idx) {
+            const int index = block_idx * THREADS_PER_BLOCK + threadIdx.x;
 
-            const int b = index / (C * H * W);
-            const int c = (index - b * C * H * W) / (H * W);
-            const int h = (index - b * C * H * W - c * H * W) / W;
-            const int w = index % W;
+            if (index < total_step && isnan(im1[index])) {
+                const int b = index / (C * H * W);
+                const int c = (index - b * C * H * W) / (H * W);
+                const int h = (index - b * C * H * W - c * H * W) / W;
+                const int w = index % W;
 
-            scalar_t sum = 0;
-            int count = 0;
+                scalar_t sum = 0;
+                int count = 0;
 
-            // Update loop bounds to consider a radius
-            for (int i = max(0, h - radius); i <= min(H - 1, h + radius); ++i) {
-                for (int j = max(0, w - radius); j <= min(W - 1, w + radius); ++j) {
-                    const int neighbor_index = get_im_index(b, c, i, j, C, H, W);
-                    if (!isnan(im1[neighbor_index])) {
-                        sum += im1[neighbor_index];
+                // Load neighboring pixels into shared memory
+                for (int i = max(0, h - radius); i <= min(H - 1, h + radius); ++i) {
+                    for (int j = max(0, w - radius); j <= min(W - 1, w + radius); ++j) {
+                        const int neighbor_index = get_im_index(b, c, i, j, C, H, W);
+                        shared_im1[count] = im1[neighbor_index];
                         ++count;
                     }
                 }
-            }
 
-            if (count > 0) im1[index] = sum / count;
-            else has_nan = true;  // Set has_nan to true if NaN pixels are found
+                __syncthreads();  // Synchronize threads before computing sum
+
+                // Compute sum using shared memory
+                for (int i = 0; i < count; ++i) {
+                    if (!isnan(shared_im1[i])) {
+                        sum += shared_im1[i];
+                    }
+                }
+
+                if (count > 0) {
+                    im1[index] = sum / count;
+                } else {
+                    has_nan = true;  // Set has_nan to true if NaN pixels are found
+                }
+            }
         }
 
-        __syncthreads();  // Add this line to synchronize threads before starting the next iteration
+        __syncthreads();  // Synchronize threads before starting the next iteration
 
         // Break out of the loop if no NaN pixels are found
-        if (!has_nan) break;
+        if (!has_nan) {
+            break;
+        }
     }
 }
+
 
 at::Tensor forward_warp_cuda_forward(
     const at::Tensor im0,
