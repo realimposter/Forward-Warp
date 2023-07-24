4be5d1fa93aff72e987f0bef16c8f24e8275c1cc
imagee <51212151+realimposter@users.noreply.github.com>
2023-07-24 00:25:33 -0700

    Update forward_warp.py

--- Forward_Warp/forward_warp.py
+++ Forward_Warp/forward_warp.py
@@ -9,7 +9,7 @@ from .python import Forward_Warp_Python
 class forward_warp_function(Function):
 
     @staticmethod
-    def forward(ctx, im0, flow, interpolation_mode):
+    def forward(ctx, im0, flow, flowback, infil_iterations, interpolation_mode):
         '''
         im0: the first image with shape [B, C, H, W]
         flow: the optical flow with shape [B, H, W, 2] (different to grid_sample, it's range is from [-W, -H] to [W, H])
@@ -20,11 +20,12 @@ class forward_warp_function(Function):
         assert(im0.shape[0] == flow.shape[0])
         assert(im0.shape[-2:] == flow.shape[1:3])
         assert(flow.shape[3] == 2)
+        assert(flowback.shape[3] == 2)
 
         ctx.interpolation_mode = interpolation_mode
         ctx.save_for_backward(im0, flow)
         if im0.is_cuda:
-            im1 = forward_warp_cuda.forward(im0, flow, interpolation_mode)
+            im1 = forward_warp_cuda.forward(im0, flow, flowback, infil_iterations, interpolation_mode)
         else:
             im1 = Forward_Warp_Python.forward(im0, flow, interpolation_mode)
 
@@ -55,6 +56,6 @@ class forward_warp(Module):
         else:
             self.interpolation_mode = 1
 
-    def forward(self, im0, flow):
+    def forward(self, im0, flow, flowback, infil_iterations):
 
-        return forward_warp_function.apply(im0, flow, self.interpolation_mode)
+        return forward_warp_function.apply(im0, flow, flowback, infil_iterations, self.interpolation_mode)
