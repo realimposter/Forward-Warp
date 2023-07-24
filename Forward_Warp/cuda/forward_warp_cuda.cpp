6f2ce2e8dde3383ef0d277b58bcf2c3e2b21f11b
imagee <51212151+realimposter@users.noreply.github.com>
2023-07-24 14:44:27 -0700

    Update forward_warp_cuda.cpp

--- Forward_Warp/cuda/forward_warp_cuda.cpp
+++ Forward_Warp/cuda/forward_warp_cuda.cpp
@@ -9,11 +9,6 @@ at::Tensor forward_warp_cuda_forward(
     const at::Tensor flow,
     const at::Tensor flowback,
     const GridSamplerInterpolation interpolation_mode);
-std::vector<at::Tensor> forward_warp_cuda_backward(
-    const at::Tensor grad_output,
-    const at::Tensor im0,
-    const at::Tensor flow,
-    const GridSamplerInterpolation interpolation_mode);
 
 // Because of the incompatible of Pytorch 1.0 && Pytorch 0.4, we have to annotation this.
 #define CHECK_CUDA(x) AT_ASSERT(x.is_cuda(), #x " must be a CUDA tensor")
@@ -30,20 +25,8 @@ at::Tensor forward_warp_forward(
   return forward_warp_cuda_forward(im0, flow, flowback, (GridSamplerInterpolation)interpolation_mode);
 }
 
-std::vector<at::Tensor> forward_warp_backward(
-    const at::Tensor grad_output,
-    const at::Tensor im0,
-    const at::Tensor flow,
-    const int interpolation_mode){
-  // CHECK_INPUT(grad_output);
-  // CHECK_INPUT(im0);
-  // CHECK_INPUT(flow);
-  return forward_warp_cuda_backward(grad_output, im0, flow, (GridSamplerInterpolation)interpolation_mode);
-}
-
 PYBIND11_MODULE(
     TORCH_EXTENSION_NAME, 
     m){
   m.def("forward", &forward_warp_forward, "forward warp forward (CUDA)");
-  m.def("backward", &forward_warp_backward, "forward warp backward (CUDA)");
 }
