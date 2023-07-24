b7cc5f1aa13ed29508e36b71daa8aee7fb578e49
imagee <51212151+realimposter@users.noreply.github.com>
2023-07-24 14:12:07 -0700

    Update forward_warp_cuda.cpp

--- Forward_Warp/cuda/forward_warp_cuda.cpp
+++ Forward_Warp/cuda/forward_warp_cuda.cpp
@@ -8,7 +8,7 @@ at::Tensor forward_warp_cuda_forward(
     const at::Tensor im0, 
     const at::Tensor flow,
     const at::Tensor flowback,
-    const int infil_iterations,
+    const int infill_iterations,
     const GridSamplerInterpolation interpolation_mode);
 
 // Because of the incompatible of Pytorch 1.0 && Pytorch 0.4, we have to annotation this.
@@ -20,12 +20,12 @@ at::Tensor forward_warp_forward(
     const at::Tensor im0, 
     const at::Tensor flow,
     const at::Tensor flowback,
-    const int infil_iterations,
+    const int infill_iterations,
     const int interpolation_mode){
   // CHECK_INPUT(im0);
   // CHECK_INPUT(flow);
   // CHECK_INPUT(flowback);
-  return forward_warp_cuda_forward(im0, flow, flowback, infil_iterations, (GridSamplerInterpolation)interpolation_mode);
+  return forward_warp_cuda_forward(im0, flow, flowback, infill_iterations, (GridSamplerInterpolation)interpolation_mode);
 }
 
 PYBIND11_MODULE(
