64f43888684aaafb0b9bcd2c5a887b24b0aa6471
imagee <51212151+realimposter@users.noreply.github.com>
2023-07-24 13:53:29 -0700

    Update forward_warp_cuda_kernel.cu

--- Forward_Warp/cuda/forward_warp_cuda_kernel.cu
+++ Forward_Warp/cuda/forward_warp_cuda_kernel.cu
@@ -110,107 +110,107 @@ __global__ void forward_warp_cuda_forward_kernel(
 
 
 
-template <typename scalar_t>
-__global__ void back_warp_kernel(
-    const int total_step,
-    const scalar_t* im0,
-    scalar_t* flow,
-    scalar_t* im1,
-    const int B,
-    const int C,
-    const int H,
-    const int W) {
-    CUDA_KERNEL_LOOP(index, total_step) {
-        const int b = index / (H * W);
-        const int h = (index-b*H*W) / W;
-        const int w = index % W;
+// template <typename scalar_t>
+// __global__ void back_warp_kernel(
+//     const int total_step,
+//     const scalar_t* im0,
+//     scalar_t* flow,
+//     scalar_t* im1,
+//     const int B,
+//     const int C,
+//     const int H,
+//     const int W) {
+//     CUDA_KERNEL_LOOP(index, total_step) {
+//         const int b = index / (H * W);
+//         const int h = (index-b*H*W) / W;
+//         const int w = index % W;
 
-        const scalar_t x = (scalar_t)w + flow[index*2+0];
-        const scalar_t y = (scalar_t)h + flow[index*2+1];
+//         const scalar_t x = (scalar_t)w + flow[index*2+0];
+//         const scalar_t y = (scalar_t)h + flow[index*2+1];
 
-        //get im1 value from im0 using flow, leaving holes as NaN
-        if (x < 0 || y < 0 || x >= W || y >= H) {
-            // Out of bound, leave as NaN
-            for (int c = 0; c < C; ++c) {
-                im1[b * (H * W * C) + c * (H * W) + h * W + w] = NAN;
-            }
-        } else {
-            // Bilinear interpolation
-            const int x1 = static_cast<int>(::floor(x));
-            const int y1 = static_cast<int>(::floor(y));
-            const int x2 = x1 + 1;
-            const int y2 = y1 + 1;
+//         //get im1 value from im0 using flow, leaving holes as NaN
+//         if (x < 0 || y < 0 || x >= W || y >= H) {
+//             // Out of bound, leave as NaN
+//             for (int c = 0; c < C; ++c) {
+//                 im1[b * (H * W * C) + c * (H * W) + h * W + w] = NAN;
+//             }
+//         } else {
+//             // Bilinear interpolation
+//             const int x1 = static_cast<int>(::floor(x));
+//             const int y1 = static_cast<int>(::floor(y));
+//             const int x2 = x1 + 1;
+//             const int y2 = y1 + 1;
 
-            const scalar_t dist_x = x - x1;
-            const scalar_t dist_y = y - y1;
+//             const scalar_t dist_x = x - x1;
+//             const scalar_t dist_y = y - y1;
 
-            for (int c = 0; c < C; ++c) {
-                // Compute weights
-                scalar_t w1 = (1 - dist_x) * (1 - dist_y);
-                scalar_t w2 = dist_x * (1 - dist_y);
-                scalar_t w3 = (1 - dist_x) * dist_y;
-                scalar_t w4 = dist_x * dist_y;
+//             for (int c = 0; c < C; ++c) {
+//                 // Compute weights
+//                 scalar_t w1 = (1 - dist_x) * (1 - dist_y);
+//                 scalar_t w2 = dist_x * (1 - dist_y);
+//                 scalar_t w3 = (1 - dist_x) * dist_y;
+//                 scalar_t w4 = dist_x * dist_y;
 
-                // Fetch pixel values
-                scalar_t p1 = (x1 < W && y1 < H) ? im0[b * (H * W * C) + c * (H * W) + y1 * W + x1] : NAN;
-                scalar_t p2 = (x2 < W && y1 < H) ? im0[b * (H * W * C) + c * (H * W) + y1 * W + x2] : NAN;
-                scalar_t p3 = (x1 < W && y2 < H) ? im0[b * (H * W * C) + c * (H * W) + y2 * W + x1] : NAN;
-                scalar_t p4 = (x2 < W && y2 < H) ? im0[b * (H * W * C) + c * (H * W) + y2 * W + x2] : NAN;
+//                 // Fetch pixel values
+//                 scalar_t p1 = (x1 < W && y1 < H) ? im0[b * (H * W * C) + c * (H * W) + y1 * W + x1] : NAN;
+//                 scalar_t p2 = (x2 < W && y1 < H) ? im0[b * (H * W * C) + c * (H * W) + y1 * W + x2] : NAN;
+//                 scalar_t p3 = (x1 < W && y2 < H) ? im0[b * (H * W * C) + c * (H * W) + y2 * W + x1] : NAN;
+//                 scalar_t p4 = (x2 < W && y2 < H) ? im0[b * (H * W * C) + c * (H * W) + y2 * W + x2] : NAN;
 
-                // Check for NaN values in the fetched pixels
-                if (isnan(p1) || isnan(p2) || isnan(p3) || isnan(p4)) {
-                    im1[b * (H * W * C) + c * (H * W) + h * W + w] = NAN;
-                } else {
-                    // Compute interpolated pixel value
-                    scalar_t px_val = p1 * w1 + p2 * w2 + p3 * w3 + p4 * w4;
-                    im1[b * (H * W * C) + c * (H * W) + h * W + w] = px_val;
-                }
-            }
-        }
-    }
-}
+//                 // Check for NaN values in the fetched pixels
+//                 if (isnan(p1) || isnan(p2) || isnan(p3) || isnan(p4)) {
+//                     im1[b * (H * W * C) + c * (H * W) + h * W + w] = NAN;
+//                 } else {
+//                     // Compute interpolated pixel value
+//                     scalar_t px_val = p1 * w1 + p2 * w2 + p3 * w3 + p4 * w4;
+//                     im1[b * (H * W * C) + c * (H * W) + h * W + w] = px_val;
+//                 }
+//             }
+//         }
+//     }
+// }
 
 
-template <typename scalar_t> __global__ void inpaint_nan_pixels_kernel(scalar_t* im1,const int B,const int C,const int H,const int W,const int infill_iterations)
-{
-    const int total_step = B * C * H * W;
-    const int radius = 4;  // Add this line to define radius
+// template <typename scalar_t> __global__ void inpaint_nan_pixels_kernel(scalar_t* im1,const int B,const int C,const int H,const int W,const int infill_iterations)
+// {
+//     const int total_step = B * C * H * W;
+//     const int radius = 4;  // Add this line to define radius
 
-    for (int iteration = 0; iteration < infill_iterations; ++iteration) {
-        bool has_nan = false;  // Track if NaN pixels are found in the iteration
+//     for (int iteration = 0; iteration < infill_iterations; ++iteration) {
+//         bool has_nan = false;  // Track if NaN pixels are found in the iteration
 
-        CUDA_KERNEL_LOOP(index, total_step) {
-            if (!isnan(im1[index])) continue;
+//         CUDA_KERNEL_LOOP(index, total_step) {
+//             if (!isnan(im1[index])) continue;
 
-            const int b = index / (C * H * W);
-            const int c = (index - b * C * H * W) / (H * W);
-            const int h = (index - b * C * H * W - c * H * W) / W;
-            const int w = index % W;
+//             const int b = index / (C * H * W);
+//             const int c = (index - b * C * H * W) / (H * W);
+//             const int h = (index - b * C * H * W - c * H * W) / W;
+//             const int w = index % W;
 
-            scalar_t sum = 0;
-            int count = 0;
+//             scalar_t sum = 0;
+//             int count = 0;
 
-            // Update loop bounds to consider a radius
-            for (int i = max(0, h - radius); i <= min(H - 1, h + radius); ++i) {
-                for (int j = max(0, w - radius); j <= min(W - 1, w + radius); ++j) {
-                    const int neighbor_index = get_im_index(b, c, i, j, C, H, W);
-                    if (!isnan(im1[neighbor_index])) {
-                        sum += im1[neighbor_index];
-                        ++count;
-                    }
-                }
-            }
+//             // Update loop bounds to consider a radius
+//             for (int i = max(0, h - radius); i <= min(H - 1, h + radius); ++i) {
+//                 for (int j = max(0, w - radius); j <= min(W - 1, w + radius); ++j) {
+//                     const int neighbor_index = get_im_index(b, c, i, j, C, H, W);
+//                     if (!isnan(im1[neighbor_index])) {
+//                         sum += im1[neighbor_index];
+//                         ++count;
+//                     }
+//                 }
+//             }
 
-            if (count > 0) im1[index] = sum / count;
-            else has_nan = true;  // Set has_nan to true if NaN pixels are found
-        }
+//             if (count > 0) im1[index] = sum / count;
+//             else has_nan = true;  // Set has_nan to true if NaN pixels are found
+//         }
 
-        __syncthreads();  // Add this line to synchronize threads before starting the next iteration
+//         __syncthreads();  // Add this line to synchronize threads before starting the next iteration
 
-        // Break out of the loop if no NaN pixels are found
-        if (!has_nan) break;
-    }
-}
+//         // Break out of the loop if no NaN pixels are found
+//         if (!has_nan) break;
+//     }
+// }
 
 at::Tensor forward_warp_cuda_forward(
     const at::Tensor im0,
@@ -243,24 +243,24 @@ at::Tensor forward_warp_cuda_forward(
     im1.div_(white_im1-1);
 
 
-    /////// WARPP BACKWARDS //////////
-    back_warp_kernel<scalar_t>
-    <<<GET_BLOCKS(total_step), CUDA_NUM_THREADS>>>(
-      total_step,
-      im0.data_ptr<scalar_t>(),
-      flowback.data_ptr<scalar_t>(),
-      im2.data_ptr<scalar_t>(),
-      B, C, H, W);
+    // /////// WARPP BACKWARDS //////////
+    // back_warp_kernel<scalar_t>
+    // <<<GET_BLOCKS(total_step), CUDA_NUM_THREADS>>>(
+    //   total_step,
+    //   im0.data_ptr<scalar_t>(),
+    //   flowback.data_ptr<scalar_t>(),
+    //   im2.data_ptr<scalar_t>(),
+    //   B, C, H, W);
 
-    //////// FILL im2 NaNs with im1 values ////////
-    im2.masked_fill_(im2.isnan(), im1);
+    // //////// FILL im2 NaNs with im1 values ////////
+    // im2.masked_fill_(im2.isnan(), im1);
 
-    /////// INPAINTING //////////
-    inpaint_nan_pixels_kernel<scalar_t>
-    <<<GET_BLOCKS(total_step), CUDA_NUM_THREADS>>>(
-      im1.data_ptr<scalar_t>(),
-      B, C, H, W,
-      infill_iterations);
+    // /////// INPAINTING //////////
+    // inpaint_nan_pixels_kernel<scalar_t>
+    // <<<GET_BLOCKS(total_step), CUDA_NUM_THREADS>>>(
+    //   im1.data_ptr<scalar_t>(),
+    //   B, C, H, W,
+    //   infill_iterations);
 
   }));
   return im1;
