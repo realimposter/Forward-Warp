9e79ea892d74dce5a1c34e8333eada622164026a
imagee <51212151+realimposter@users.noreply.github.com>
2023-07-26 15:21:06 -0700

    Update forward_warp_cuda.cpp

--- Forward_Warp/cuda/forward_warp_cuda.cpp
+++ Forward_Warp/cuda/forward_warp_cuda.cpp
@@ -11,7 +11,8 @@ at::Tensor forward_warp_cuda_forward(
     const GridSamplerInterpolation interpolation_mode,
     const int inpaint_search_radius,
     const float inpaint_motion_threshold,
-    const int max_iterations);
+    const int max_iterations,
+    const int mask_dilation);
 
 // Because of the incompatible of Pytorch 1.0 && Pytorch 0.4, we have to annotation this.
 #define CHECK_CUDA(x) AT_ASSERT(x.is_cuda(), #x " must be a CUDA tensor")
@@ -25,10 +26,11 @@ at::Tensor forward_warp_forward(
     const int interpolation_mode,
     const int inpaint_search_radius,
     const float inpaint_motion_threshold,
-    const int max_iterations){
+    const int max_iterations,
+    cosnt int mask_dilation){
   // CHECK_INPUT(im0);
   // CHECK_INPUT(flow);
-  return forward_warp_cuda_forward(im0, flow, flowback, (GridSamplerInterpolation)interpolation_mode, inpaint_search_radius, inpaint_motion_threshold,max_iterations);
+  return forward_warp_cuda_forward(im0, flow, flowback, (GridSamplerInterpolation)interpolation_mode, inpaint_search_radius, inpaint_motion_threshold,max_iterations,mask_dilation);
 }
 
 PYBIND11_MODULE(
