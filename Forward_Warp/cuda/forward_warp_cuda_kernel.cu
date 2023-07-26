24e7b4c65c93a5d99ef0f70ed1b5c9ac86bc4434
imagee <51212151+realimposter@users.noreply.github.com>
2023-07-25 21:38:32 -0700

    Update forward_warp_cuda_kernel.cu

--- Forward_Warp/cuda/forward_warp_cuda_kernel.cu
+++ Forward_Warp/cuda/forward_warp_cuda_kernel.cu
@@ -225,7 +225,7 @@ __global__ void inpaint_nan_pixels_kernel(
                     const scalar_t flowDiff = abs(x1 - x2) + abs(y1 - y2);
 
                     // if the flows are different, move on to the next neighbor. disable this while testing
-                    if (flowDiff > 2) continue;
+                    if (flowDiff > 0.01) continue;
 
                     // else copy the neighbor pixel value to the current pixel
                     scalar_t* im0_p = im0 + get_channel_index(b, 0, h, w, C, H, W);
